03839fe4f42ffdf8c631918482623db48e221419
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2025-02-10 13:44:30 +0100

    feat(modules/evm): add `JsonTestReporter`

--- modules/evm/hardhat.config.ts
+++ modules/evm/hardhat.config.ts
@@ -1,11 +1,12 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { JsonTestReporter } from "@shared/mocha/reporters/json";
 
 const config: HardhatUserConfig = {
     solidity: "0.8.20",
     networks: {
         xrplevm_localnet: {
-            url: "http://localhost:8545",
+            url: process.env.RPC_URL || "http://localhost:8545",
             accounts: [
                 process.env.CONTRACT_PRIVATE_KEY || "1bacb3ef3d80d8f148299bd21a56d9a78048dc2e20ddab25713a408f2d1f4d5e",
                 process.env.USER_PRIVATE_KEY || "d8161fd5fdf4eb216a6556dc639a2543b0ed9abdbceb9971fe08cfba56f588c8",
@@ -15,7 +16,7 @@ const config: HardhatUserConfig = {
     },
     mocha: {
         timeout: 40000,
-        // reporter: JsonTestReporter,
+        reporter: JsonTestReporter,
     },
 };
 
