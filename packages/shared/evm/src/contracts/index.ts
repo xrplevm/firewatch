e9ce8f5a421502fd2b90fb5a832920b8004976a7
banasa44 <cfbennassar@gmail.com>
2025-03-05 16:56:53 +0100

    feat(axelar): test for interchain token transfers

--- packages/shared/evm/src/contracts/index.ts
+++ packages/shared/evm/src/contracts/index.ts
@@ -1,5 +1,6 @@
 export * from "./erc20";
 export * from "./interchain-token-service";
 export * from "./interchain-token-factory";
+export * from "./interchain-erc20";
 export * from "./axelar-amplifier-gateway";
 export * from "./call-contract";
--- packages/shared/evm/src/contracts/interchain-erc20.ts
+++ packages/shared/evm/src/contracts/interchain-erc20.ts
@@ -5,11 +5,23 @@ import { Contract } from "./contract";
 export const interchainTokenAbi = [
     "function interchainTokenService() view returns (address)",
     "function interchainTokenId() view returns (bytes32)",
+    "function interchainTransfer(string destinationChain, bytes recipient, uint256 amount, bytes metadata) external payable",
 ];
 
 export interface IInterchainToken extends IERC20 {
     interchainTokenService(): Promise<string>;
     interchainTokenId(): Promise<string>;
+
+    interchainTransfer(
+        destinationChain: string,
+        recipient: string, // Hex string representing the recipient bytes
+        amount: ethers.BigNumberish,
+        metadata: string,
+        options?: {
+            gasLimit?: number;
+            value?: ethers.BigNumberish;
+        },
+    ): Promise<ethers.ContractTransactionResponse>;
 }
 
 export class InterchainToken extends Contract<IInterchainToken> {
