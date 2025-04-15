04b4015c5c4bf0413ba02b405a510e20aad66235
banasa44 <cfbennassar@gmail.com>
2025-04-15 12:17:16 +0200

    feat(cosmos): PoC structure, add height to cosmosChain

--- modules/cosmos/src/models/chain.ts
+++ modules/cosmos/src/models/chain.ts
@@ -3,6 +3,7 @@ import { Chain } from "@firewatch/core/chain";
 import { CosmosModule } from "../models/module";
 
 export interface CosmosChain extends Chain {
+    height: number;
     module: CosmosModule;
     account: Pick<Account, "privateKey">;
 }
