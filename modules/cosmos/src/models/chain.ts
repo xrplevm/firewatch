1d8940f0e2162c6de694160c73dc370ebfee91d5
banasa44 <cfbennassar@gmail.com>
2025-04-16 12:52:13 +0200

    feat(cosmos): remove height from cosmos.chain config

--- modules/cosmos/src/models/chain.ts
+++ modules/cosmos/src/models/chain.ts
@@ -3,7 +3,6 @@ import { Chain } from "@firewatch/core/chain";
 import { CosmosModule } from "../models/module";
 
 export interface CosmosChain extends Chain {
-    height: number;
     module: CosmosModule;
     account: Pick<Account, "name">;
 }
