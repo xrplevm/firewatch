cb21d1fd4f34a4cbc12ff313a334925e0550fb7f
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2025-05-06 18:21:41 +0200

    feat(testing/mocha): add skippable `describe` and `it`

--- packages/testing/mocha/src/assertions/chain.ts
+++ packages/testing/mocha/src/assertions/chain.ts
@@ -1,7 +1,6 @@
 import { ChainType } from "@shared/modules/chain";
 import { Chain } from "@firewatch/core/chain";
 import { Env } from "../../../../env/src/types/env";
-import { AssertionErrors } from "./errors";
 
 /**
  * Validates the chain types of the source and destination chains.
@@ -9,12 +8,8 @@ import { AssertionErrors } from "./errors";
  * @param chain The chain to validate.
  * @returns True if the chain types are valid, false otherwise.
  */
-export function assertChainTypes<C extends Chain>(allowedChainTypes: ChainType[], chain: C): boolean {
-    if (!allowedChainTypes.includes(chain.type)) {
-        throw new Error(AssertionErrors.CHAIN_TYPE_MISMATCH);
-    }
-
-    return true;
+export function isChainType<C extends Chain>(allowedChainTypes: ChainType[], chain: C): boolean {
+    return allowedChainTypes.includes(chain.type);
 }
 
 /**
@@ -23,10 +18,6 @@ export function assertChainTypes<C extends Chain>(allowedChainTypes: ChainType[]
  * @param chain The chain to validate.
  * @returns True if the environments are valid, false otherwise.
  */
-export function assertChainEnvironments<C extends Chain>(allowedEnvironments: Env[], chain: C): boolean {
-    if (!allowedEnvironments.includes(chain.env)) {
-        throw new Error(AssertionErrors.CHAIN_ENVIRONMENT_MISMATCH);
-    }
-
-    return true;
+export function isChainEnvironment<C extends Chain>(allowedEnvironments: Env[], chain: C): boolean {
+    return allowedEnvironments.includes(chain.env);
 }
