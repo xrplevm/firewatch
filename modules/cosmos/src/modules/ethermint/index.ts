6cd47a24a23fd2742eb1dbee09e0a2d703536852
Kevin Pita <kpita@peersyst.com>
2026-04-23 15:47:49 +0200

    feat(cosmos): extract ethermint module and add gov/query helpers

---
+++ modules/cosmos/src/modules/ethermint/index.ts
@@ -0,0 +1,7 @@
+export * from "./account-parser";
+export * from "./parser";
+export * from "./pubkey";
+export * from "./secp256k1";
+export * from "./signer";
+export * from "./signing-client";
+export * from "./signing-stargate-client";
